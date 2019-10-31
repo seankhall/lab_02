e7f0209da3c91c54dc692a9a65e5a8ad392485aa
jericam832 <jericam32@gmail.com>
2019-10-31 12:02:09 -0700

    pagination completed

---
+++ js/page2.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+  $.get('data/page-2.json')
+    .then(hornImg => {
+      hornImg.forEach(data => {
+        $('#photo-template2').append(`<div value="${data.keyword}"><h2>${data.title}</h2><img src="${data.image_url}"><p>${data.description}</p></div>`);
+        let createOpt = $('<option></option>');
+        createOpt.attr('value', `${data.keyword}`);
+        createOpt.text(`${data.keyword}`);
+        $('select').append(createOpt);
+      });
+      //   console.log(hornImg);
+      //   console.log(createOpt);
+    })
+
+  $('select').on('change', function () {
+    let selectedValue = $(this).val();
+    $('div').hide();
+    $(`div[value=${selectedValue}]`).show();
+    console.log(selectedValue);
+  });
+});
